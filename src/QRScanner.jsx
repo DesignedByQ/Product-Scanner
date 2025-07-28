50210e7299346f100433958bf9311e7581fd1678
Henry Opara <henry.o@hotmail.co.uk>
2025-07-28 16:41:28 +0100

    fixed camera19

--- src/QRScanner.jsx
+++ src/QRScanner.jsx
@@ -3,28 +3,33 @@ import { Html5QrcodeScanner } from 'html5-qrcode';
 
 function QRScanner({ onScan }) {
 
+  const [scanResult, setScanResult] = useState(null);
+
   useEffect(() => {
+
+  function success(result) {
+    //scanner.clear();
+    setScanResult(result);
+    console.log("QR Code scan result: ", result);
+    console.log("QR Code scan result: ", scanResult);
+    if (onScan) {
+      onScan(scanResult); // Pass scanned data to parent (App)
+    }
+  }
+
+  function error(err) {
+    console.warn("QR Code scan error: ", err);
+  }
 
     const scanner = new Html5QrcodeScanner(
     "qr-scanner", {
       qrbox: { width: 250, height: 250 },
       rememberLastUsedCamera: true,
       fps: 5,
-    });
+  });
 
-    scanner.render(success, error);
+  scanner.render(success, error);
 
-    function success(decodedText) {
-      // Pass scanned data to parent
-      if (onScan) {
-        onScan(decodedText);
-      }
-    }
-
-  function error(err) {
-    console.warn("QR Code scan error: ", err);
-  }
- 
   }, [])
 
     return <div id="qr-scanner"></div>;
